de0ffc7669fd7aed1ed9d76a008e6182aaa79bdb
JunCastle <juncastle97@gmail.com>
2025-03-31 12:58:22 +0900

    feat : day 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 import SearchBar from "@/components/SearchBar";
+import dummy from "@/mock/dummy.json";
+import MovieItem from "@/components/MovieItem";
+import style from "./index.module.css";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <section>
+        <h3>지금 가장 추천하는 영화</h3>
+        <div className={style.recommendMovie}>
+          {dummy.slice(0, 3).map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <div className={style.allMovie}>
+          {dummy.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 Home.getLayout = (page: React.ReactNode) => {
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,17 @@
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "next/router";
+import dummy from "@/mock/dummy.json";
+import MovieItem from "@/components/MovieItem";
+import style from "./index.module.css";
 
 export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
-
-  return <h1>검색결과 : {q}</h1>;
+  return (
+    <div className={style.container}>
+      {dummy.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: React.ReactNode) => {
